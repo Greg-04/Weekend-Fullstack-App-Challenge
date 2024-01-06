2b47a3acc79cbeb01fe57f207c8e8bc429247d87
Gabriel Regalado <regs0404@gmail.com>
2024-01-06 16:17:27 -0500

    added in get route

--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -5,7 +5,7 @@ const pool = require('../modules/pool.js');
 // GET
 
 router.get('/', (req, res) => {
-    const dbQuery = 'SELECT * FROM "koalas";';
+    const dbQuery = 'SELECT * FROM "toDoList";';
     pool
         .query(dbQuery)
         .then((result) => {
