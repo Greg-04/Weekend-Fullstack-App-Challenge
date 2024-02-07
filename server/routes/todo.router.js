fc0f5f73714bb77e712be481d01f5a47bc5aa597
Gabriel Regalado <regs0404@gmail.com>
2024-02-06 20:31:23 -0500

    updated server

--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -5,58 +5,55 @@ const pool = require('../modules/pool.js');
 // GET
 
 router.get('/', (req, res) => {
-    const dbQuery = 'SELECT * FROM "toDoList" ORDER BY "id" ASC;';
-    pool
-        .query(dbQuery)
-        .then((result) => {
-            console.log('RESULT', result);
-            res.send(result.rows);
-        })
-        .catch((err) => {
-            console.log('ERROR', err);
+  const dbQuery = 'SELECT * FROM "toDoList" ORDER BY "id" ASC;';
+  pool
+    .query(dbQuery)
+    .then((result) => {
+      console.log('RESULT', result);
+      res.send(result.rows);
+    })
+    .catch((err) => {
+      console.log('ERROR', err);
 
-            res.sendStatus(500);
-        });
+      res.sendStatus(500);
+    });
 });
 
 // POST
 
 router.post('/', (req, res) => {
-    const newTask = req.body;
-    const queryText = `
+  const newTask = req.body;
+  const queryText = `
     INSERT INTO "toDoList" ("Task", "Description")
     VALUES 
 	($1, $2);`;
-    const queryArgs = [
-        newTask.Task,
-        newTask.Description,
-    ];
-    pool    
-        .query(queryText, queryArgs)
-        .then((results) => {
-            res.sendStatus(201);
-        })
-        .catch((err) => {
-            res.sendStatus(500);
-        });
+  const queryArgs = [newTask.Task, newTask.Description];
+  pool
+    .query(queryText, queryArgs)
+    .then((results) => {
+      res.sendStatus(201);
+    })
+    .catch((err) => {
+      res.sendStatus(500);
+    });
 });
 
 // PUT
 
 router.put('/:id', (req, res) => {
-    const id = Number(req.params.id);
+  const id = Number(req.params.id);
 
-    // const updatedTaskDescription = req.body.Description;
-    // console.log('req.body.description', updatedTaskDescription)
+  // const updatedTaskDescription = req.body.Description;
+  // console.log('req.body.description', updatedTaskDescription)
 
-    // if (updatedTaskDescription === undefined) {
-    //     return res.status(400).send('Invalid data provided');
-    // }
+  // if (updatedTaskDescription === undefined) {
+  //     return res.status(400).send('Invalid data provided');
+  // }
 
-    //old queryText
-//   let queryText = `UPDATE "toDoList" SET "Description" = $1 WHERE "id" = $2;`;
+  //old queryText
+  //   let queryText = `UPDATE "toDoList" SET "Description" = $1 WHERE "id" = $2;`;
 
-    let queryText = `UPDATE "toDoList" SET "Status" = NOT "Status" WHERE "id" = $1;`;    
+  let queryText = `UPDATE "toDoList" SET "Status" = NOT "Status" WHERE "id" = $1;`;
 
   pool
     // .query(queryText, [updatedTaskDescription, id])
@@ -73,19 +70,19 @@ router.put('/:id', (req, res) => {
 // DELETE
 
 router.delete('/:id', (req, res) => {
-    const id = Number(req.params.id);
+  const id = Number(req.params.id);
 
-    const queryText = 'DELETE FROM "toDoList" WHERE "id" = $1;';
+  const queryText = 'DELETE FROM "toDoList" WHERE "id" = $1;';
 
-    pool
-        .query(queryText, [id])
-        .then((results) => {
-            res.sendStatus(200);
-        })
-        .catch((err) => {
-            console.error(`Error: making database query ${queryText}`, err);
-            res.sendStatus(500); // good server always responds
-        });
+  pool
+    .query(queryText, [id])
+    .then((results) => {
+      res.sendStatus(200);
+    })
+    .catch((err) => {
+      console.error(`Error: making database query ${queryText}`, err);
+      res.sendStatus(500); // good server always responds
+    });
 });
 
 module.exports = router;
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,16 @@
-import {useState , useEffect} from 'react';
-import {fetchTasks , deleteTask, updateTaskStatus} from '../tasksAPI/tasks.api';
+import { useState, useEffect } from 'react';
+import {
+  fetchTasks,
+  deleteTask,
+  updateTaskStatus,
+} from '../tasksAPI/tasks.api';
 import AddTaskForm from '../AddTaskForm/AddTaskForm';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { red } from '@mui/material/colors';
 import Button from '@mui/material/Button';
 import './App.css';
 
-function App () {
+function App() {
   const [toDoList, setToDoList] = useState([
     // {Task: 'Task 1', Description: 'Clean House'},
     // {Task: 'Task 2', Description: 'Wash Car'},
@@ -25,71 +29,92 @@ function App () {
     // api call
     const taskPromise = fetchTasks();
     taskPromise
-    //success
-    .then((response) => {
-      console.log('SERVER DATA:', response);
-      setToDoList(response.data);
-  })
-  //failure
-  .catch((err) => {
-      console.error('ERROR:', err);
-  });
-};
+      //success
+      .then((response) => {
+        console.log('SERVER DATA:', response);
+        setToDoList(response.data);
+      })
+      //failure
+      .catch((err) => {
+        console.error('ERROR:', err);
+      });
+  };
 
-   // initial load of component
-   useEffect(() => {
+  // initial load of component
+  useEffect(() => {
     // body of effect
     console.log('Hi Hi');
     // api call
     refreshTasks();
   }, []);
 
-    //delete function
-    const handleClickDelete = (taskId) => {
-      //ID item
-      console.log('DELETE - taskId:', taskId);
-      //Make Axios Call
-      deleteTask(taskId)
+  //delete function
+  const handleClickDelete = (taskId) => {
+    //ID item
+    console.log('DELETE - taskId:', taskId);
+    //Make Axios Call
+    deleteTask(taskId)
       .then((response) => {
         refreshTasks();
       })
       .catch((err) => {
         console.error('ERROR:', err);
       });
-    };
-  
-    const handleTaskStatus = (id) => {
-      updateTaskStatus(id)
+  };
+
+  const handleTaskStatus = (id) => {
+    updateTaskStatus(id)
       .then((response) => {
         refreshTasks();
       })
       .catch((err) => {
         console.error('ERROR:', err);
       });
-    };
+  };
 
   return (
     <div>
       <h1 className="title">TO DO APP</h1>
-      <AddTaskForm taskRefreshCallback = {refreshTasks}/>
+      <AddTaskForm taskRefreshCallback={refreshTasks} />
       {toDoList.map((taskData, dataIndex) => {
-          return (
-            <div key={dataIndex} className={taskData.Status ? 'Completed' : 'Uncompleted'}>
-            <h3> Task {dataIndex + 1}: {taskData.Task}</h3>
+        return (
+          <div
+            key={dataIndex}
+            className={taskData.Status ? 'Completed' : 'Uncompleted'}>
+            <h3>
+              {' '}
+              Task {dataIndex + 1}: {taskData.Task}
+            </h3>
             <div>
-            <p> <span className="Span">Description:</span> {taskData.Description}</p>
+              <p>
+                {' '}
+                <span className="Span">Description:</span>{' '}
+                {taskData.Description}
+              </p>
             </div>
-            <div> <span className="Span">Status: </span> <i>"{taskData.Status ? 'Completed' : 'Uncompleted'}"</i></div>
-            <p><input type="checkbox" onClick={(event) => handleTaskStatus(taskData.id)}></input> </p>
+            <div>
+              {' '}
+              <span className="Span">Status: </span>{' '}
+              <i>"{taskData.Status ? 'Completed' : 'Uncompleted'}"</i>
+            </div>
+            <p>
+              <input
+                type="checkbox"
+                onClick={(event) => handleTaskStatus(taskData.id)}></input>{' '}
+            </p>
             <ThemeProvider theme={theme}>
-            <Button size="small" variant="contained" onClick={(event) => handleClickDelete(taskData.id)}>Delete</Button>
+              <Button
+                size="small"
+                variant="contained"
+                onClick={(event) => handleClickDelete(taskData.id)}>
+                Delete
+              </Button>
             </ThemeProvider>
-            </div>
-          );
+          </div>
+        );
       })}
     </div>
   );
-
 }
 
-export default App
+export default App;
